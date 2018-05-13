253ffb266bf4b5343eda003dc193ce438f59de7c
sfy <373406811@qq.com>
2018-05-13 21:41:52 +0800

    粗糙的前台往数据库传值访问路径/login

--- src/main/java/com/stream/changjiang/controller/TestController.java
+++ src/main/java/com/stream/changjiang/controller/TestController.java
@@ -4,14 +4,19 @@ import com.stream.changjiang.dao.entity.Account;
 import com.stream.changjiang.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Date;
+import java.util.UUID;
 
 @RestController
 public class TestController {
+
     @Autowired
     private AccountService accountServiceImpl;
+
     @RequestMapping("/test")
     public Account getTest(){
         Account account = new Account();
@@ -24,5 +29,36 @@ public class TestController {
         accountServiceImpl.saveAccount(account);
         Account account1 = accountServiceImpl.queryAccount(1L);
         return account1;
+    }
+
+    @RequestMapping("/firstFreemarker")
+    public ModelAndView firstFreemarker(){
+        String name = "I am a first Freemarker proceture !";
+        System.out.print(name);
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("name",name);
+        mav.setViewName("/firstFreemarker");
+        return mav;
+    }
+
+    @RequestMapping("/login")
+    public ModelAndView login(){
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("/login");
+        return mav;
+    }
+
+    @RequestMapping("/addAccount")
+    @ResponseBody
+    public String addAccount(Account account){
+
+        System.out.print("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
+
+        account.setId(100002l);
+        account.setCreateTime(new Date());
+        account.setPhone("15000000000");
+        accountServiceImpl.saveAccount(account);
+        String result = "success";
+        return result;
     }
 }
