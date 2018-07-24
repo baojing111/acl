4d53b5164bb63836d84a5c7ea37765d4eabb4b9b
baojing <wb-bj355912@alibaba-inc.com>
2018-07-24 11:18:01 +0800

    '日常提交'

--- src/main/java/com/stream/changjiang/ChangjiangApplication.java
+++ src/main/java/com/stream/changjiang/ChangjiangApplication.java
@@ -3,7 +3,9 @@ package com.stream.changjiang;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.stream.changjiang.*"})
 public class ChangjiangApplication {
