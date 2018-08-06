1d59d31245cb13320264dc563c27f32c10c4cd93
chenji <799743554@qq.com>
2018-08-06 23:38:29 +0800

    dd

--- src/main/java/com/stream/changjiang/controller/TestController.java
+++ src/main/java/com/stream/changjiang/controller/TestController.java
@@ -1,26 +1,26 @@
 package com.stream.changjiang.controller;
 
-import com.stream.changjiang.dao.entity.Account;
-import com.stream.changjiang.service.AccountService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiParam;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.ModelAndView;
 import study.chenji.poi.ExcelUtil;
 import study.chenji.poi.TestAccount;
-
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
+
 @Api("人员技能")
 @RestController
 @RequestMapping("/api/empSkill")
 public class TestController {
+
+    public String testMvc(String name){
+        System.out.println(name + "========================");
+        return name;
+
+    }
 
 
    @PostMapping("import")
